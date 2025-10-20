128d305ce97e59d5692db1078ed092914efbaca1
Raymond Zou <raymondzou12345@gmail.com>
2025-10-19 23:41:40 -0400

    Retrieve real and effective user IDs
    Added user ID retrieval for security checks.

--- lab2/vulp.c
+++ lab2/vulp.c
@@ -7,16 +7,19 @@ int main()
     char * fn = "/tmp/XYZ";
     char buffer[60];
     FILE *fp;
-
+    uid_t ruid = getuid();
+    uid_t euid = geteuid(); 
     /* get user input */
     scanf("%50s", buffer );
 
-   if(!access(fn, W_OK)){ 
+   if(!access(fn, W_OK)){
+       //sleep(20);  
        fp = fopen(fn, "a+"); 
        fwrite("\n", sizeof(char), 1, fp);
        fwrite(buffer, sizeof(char), strlen(buffer), fp);
        fclose(fp);
    }
    else printf("No permission \n");
+   seteuid(euid);
 }
 
